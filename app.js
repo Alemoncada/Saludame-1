7e98c80148c3747671da267c0fb6d588284255f2
Alexis Moncada <alecabj12@gmail.com>
2020-09-03 19:31:17 -0500

    saludame 1

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
 if (req.query.nombre){
     res.send("<h1>Hola " + req.query.nombre+"!</h1>");
 }
-res.send("<h1> Hola desconocido!</h1>"); 
+res.send("<h1>Hola desconocido!</h1>"); 
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'))
